2289bc5a48efa3528e2e24fe1f000efa96e7dc8f
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-12 11:48:31 -0300

    Criando rotas

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-
+import Landing from './pages/Landing';
+import OrphanagesMap from './pages/OrphanagesMap';
 export default function routes() {
     return (
         <div>
             <BrowserRouter>
-            <Route path="/" component={}/>
+            <Route path="/" component={Landing}/>
+            <Route path="/app" component={OrphanagesMap}/>
             </BrowserRouter>
         </div>
     )
