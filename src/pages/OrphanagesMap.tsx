4ddc73d322d5f17de5748cdd912bf3727dac1207
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-12 12:31:53 -0300

    Finalizando pagina de mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -24,7 +24,8 @@ export default function OrphanagesMap() {
             </aside>
 
             <Map center={[-19.8781429, - 43.9268517]} zoom={15} style={{ width: '100%', height: '100%' }}>
-                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
             </Map>
 
 
