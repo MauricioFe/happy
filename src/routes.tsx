ca75860579d551dc707c319c062b0b94f11679b8
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-12 11:42:30 -0300

    Componentizando o projeto

---
+++ src/routes.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+export default function routes() {
+    return (
+        <div>
+            <BrowserRouter>
+            <Route path="/" component={}/>
+            </BrowserRouter>
+        </div>
+    )
+}
