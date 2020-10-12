8b7e9accda5b4841c6b3ea434c33d21131d27199
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-12 11:55:58 -0300

    Criando a side bar

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 
 export default function OrphanagesMap() {
     return (
-        <div>
-            <h1>Hello</h1>
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMakerImg} alt="Happy" />
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Belo Horizonte</strong>
+                    <span>Minas Gerais</span>
+                </footer>
+            </aside>
         </div>
     )
 }
