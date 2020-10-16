58a921a5242ce4a25dd5ae918a01accfe121c3f0
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-16 13:42:58 -0300

    Colocando o maker no mapa

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import mapMakerImg from '../images/map-marker.svg';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
-import { Map, TileLayer, Marker } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-
+import leaflet from 'leaflet';
 import '../styles/pages/orphanages-map.css';
 
+
+const mapIcon = leaflet.icon({
+    iconUrl: mapMakerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 export default function OrphanagesMap() {
     return (
@@ -24,8 +31,19 @@ export default function OrphanagesMap() {
             </aside>
 
             <Map center={[-19.8781429, - 43.9268517]} zoom={15} style={{ width: '100%', height: '100%' }}>
-                
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
+
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3dhcmxleSIsImEiOiJja2djZ2dmaWcwZTQ4MnJwNDJwdWc3a3NyIn0.PiLeBjPCkil_D59b6pIOVg`} />
+                <Marker
+                    position={[-19.8781429, - 43.9268517]}
+                    icon={mapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Lar das meninas
+                        <Link to="">
+                            <FiArrowRight size={20} color="#fff" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
 
