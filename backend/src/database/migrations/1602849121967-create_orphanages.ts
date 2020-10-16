f954a45023047ad1b8e07e944a162f7523ee0fb4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-16 09:03:34 -0300

    Criando tabela orphanages

---
+++ backend/src/database/migrations/1602849121967-create_orphanages.ts
@@ -0,0 +1,54 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createOrphanages1602849121967 implements MigrationInterface {
+    // Realizar alterações na nossa tabela
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(new Table({
+            name: 'orphanages',
+            columns: [
+                {
+                    name: 'id',
+                    type: 'integer',
+                    unsigned: true,
+                    isPrimary: true,
+                    isGenerated: true,
+                    generationStrategy: 'increment'
+                },
+                {
+                    name: 'name',
+                    type: 'varchar',
+                },
+                {
+                    name: 'latitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2,
+                },
+                {
+                    name: 'longitude',
+                    type: 'decimal',
+                    scale: 10,
+                    precision: 2,
+                },
+                {
+                    name: 'about',
+                    type: 'text',
+                },
+                {
+                    name: 'instructions',
+                    type: 'text',
+                },
+                {
+                    name: 'open_on_weekends',
+                    type: 'boolean',
+                    default: false,
+                },
+            ]
+        }))
+    }
+    //rollback ou seja vai desfazer tudo que foi feito no up
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('orphanages');
+    }
+
+}
